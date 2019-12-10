bf584454199fbd8a91a4bb91464466bebbe691e6
Andreas <t-andreas.tihor@ovo.id>
2019-12-10 14:15:02 +0700

    added change value price per link / hour

--- app/controller/database.js
+++ app/controller/database.js
@@ -2,17 +2,24 @@ const database_products = require('../../database/products')
 
 
 module.exports.write = (link, name,price,description,images) => {
-  //write function to database
-  database_products.products.push({
-    link,
+  const object = {}
+  object[link] = {
     name,
     price,
     description,
     images,
-  })
+  }
+  //write function to database
+  database_products.products.push(object)
   return database_products
 }
 
 module.exports.getAllProducts = () => {
   return database_products.products
+}
+
+module.exports.changePriceValuePerLink = (link, price) => {
+  const tempObject = database_products.products
+  tempObject[link].price = price
+  return 
 }
--- app/controller/fabelio.js
+++ app/controller/fabelio.js
@@ -4,10 +4,19 @@ const database = require('./database')
 module.exports.getDetail = async (request, h) => {
   const link = request.payload.link
   const {name, price, description, images} = await fabelio.getDetailInformation(link)
+  changeValuePerHour(link)
   database.write(link, name,price,description,images)
   return "Link Confirmed"
 }
 
 module.exports.getAllProducts = (request,h) => {
   return database.getAllProducts()
+}
+
+async function changeValuePerHour(link) {
+  const ONEHOUR = 60 * 60 * 1000; // 60 minutes
+    setInterval( async() => {
+    const price = await fabelio.getPriceBaseOnLink(link)
+    return database.changePriceValuePerLink(link, price)
+  },ONEHOUR)
 }
