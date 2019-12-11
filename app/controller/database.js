3c847b3568f23aebff3619092dd8e60c6d26f05d
Andreas <t-andreas.tihor@ovo.id>
2019-12-11 10:05:08 +0700

    added price log

--- app/controller/database.js
+++ app/controller/database.js
@@ -1,16 +1,17 @@
 const database_products = require('../../database/products')
-
+const moment_timezone = require('moment-timezone')
 
 module.exports.write = (link, name,price,description,images) => {
   const object = {}
-  object[link] = {
+  //write function to database
+  database_products.products.push({
+    link,
     name,
     price,
     description,
     images,
-  }
-  //write function to database
-  database_products.products.push(object)
+    priceHistory : []
+  })
   return database_products
 }
 
@@ -19,7 +20,19 @@ module.exports.getAllProducts = () => {
 }
 
 module.exports.changePriceValuePerLink = (link, price) => {
-  const tempObject = database_products.products
-  tempObject[link].price = price
-  return 
+
+  for(let i =0, n = database_products.products.length; i<n; i++) {
+    const tempObject = database_products.products[i]
+    if (tempObject.link == link) {
+      const prevPrice = tempObject.price
+      const time = moment_timezone().tz('Jakarta')
+      tempObject.priceHistory.push({
+        time,
+        prevPrice,
+      })
+      tempObject.price = price
+      break
+    }
+  }
+  return
 }
